360613baf6da08bd824f83040ed76d5325ffbcfc
Tuğçe Nur <56029547+kilicarslantugce@users.noreply.github.com>
2021-10-28 12:28:09 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,6 +54,8 @@ Rails.application.configure do
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
+  
+  config.log_level = :debug
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
