1b3a9ef9346c68c7f5a57941a7fff87b9a36b310
Tuğçe Nur <56029547+kilicarslantugce@users.noreply.github.com>
2021-10-28 12:35:31 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
   
-  config.set HEROKU_DEBUG_RAILS_RUNNER=1 
+  
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
